3d6a759ad59d63a74c662a7cc767d9d262f4e68c
zennyMe17 <gowad.hemanth.1718@gmail.com>
2024-11-07 18:50:19 +0530

    Form

--- form/src/App.js
+++ form/src/App.js
@@ -19,31 +19,94 @@ function App() {
   //   setLastName(event.target.value);
   // }
 
-  const [formData, setFormData] = useState({firstName:"",lastName:"",email:""});
-  console.log(formData.email)
+  const [formData, setFormData] = useState({firstName:"",lastName:"",email:"",comments:"",isVisible:"True",status:"",favCar:""});
+  // console.log(formData);
   function changeHandler(event){
+
+    const{name,value,checked,type} = event.target
     setFormData(prevFormData =>{
+      
       return{
         ...prevFormData,
-        [event.target.name] : event.target.value
+        [name] : type === "checkbox" ? checked : value
       }
     });
+  }
+  
+  function submitHandler(event){
+    event.preventDefault();
+    console.log(formData);
   }
 
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <input type='text' placeholder='first name'
         onChange={changeHandler}
-        name = "firstName"/>
+        name = "firstName"
+        value={formData.firstName}/>
         <br/><br/>
         <input type='text' placeholder='Last name'
         onChange={changeHandler}
-        name = "lastName"/>
+        name = "lastName"
+        value={formData.lastName}/>
         <br/><br/>
         <input type='email' placeholder='Enter mail'
         onChange={changeHandler}
-        name='email'/>
+        name='email'
+        value={formData.email}/>
+        <br/><br/>
+        <textarea
+        placeholder='Enter Your Comments'
+        onChange={changeHandler}
+        name='comments'
+        value={formData.comments}/>
+        <br/><br/>
+        <input type='checkbox'
+        onChange={changeHandler}
+        name='isVisible'
+        id='isVisible'
+        checked={formData.isVisible}/>
+        <label htmlFor='isVisible'>Am I isVisible</label>
+        <br/><br/>
+
+
+        
+
+
+      <fieldset>
+        <legend>Mode</legend>
+        <input type='radio'
+        onChange={changeHandler}
+        name='status'
+        value="Online-Mode"
+        id='Online-mode'
+        checked={formData.status === "Online-mode"}/>
+        <label 
+        htmlFor='Online-mode'>Online Mode</label>
+        <input type='radio'
+        onChange={changeHandler}
+        name='status'
+        value="Offline-Mode"
+        id='Offline-mode'
+        checked={formData.status === "Offline-mode"}/>
+        <label 
+        htmlFor='Offline-mode'>Offline Mode</label>
+        <br/><br/>
+      </fieldset>
+
+    <select onChange={changeHandler}
+    name="favCar"
+    value={formData.favCar}
+    id='favCar'
+    >
+    <option value = "Scarpio">Scarpio</option>
+    <option value = "Thar">Thar</option>
+      
+    </select>
+
+    <button>Submit</button>
+
       </form>
     </div>
   );
